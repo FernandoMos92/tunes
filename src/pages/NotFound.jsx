1f207bf6e909cf95b8cd0698c0a60ca2ab9d0f3d
FernandoMos92 <fernandomosp@gmail.com>
2021-11-16 16:34:32 -0300

    feat: First task completed. Add all Route and create all files in Pages folder

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found"> NOT FOUND </div>
+    );
+  }
+}
+
+export default NotFound;
