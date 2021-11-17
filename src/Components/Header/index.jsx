92115075b2a68269b5251d5b5e1c4a9f643e394f
FernandoMos92 <fernandomosp@gmail.com>
2021-11-17 12:57:06 -0300

    feat: create elemente header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <header data-testid="header-component"> </header>
+    );
+  }
+}
+
+export default index;
