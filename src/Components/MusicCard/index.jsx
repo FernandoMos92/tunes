badb77060e76bea00e8645b5eabb929170f9dc66
FernandoMos92 <fernandomosp@gmail.com>
2021-11-21 15:38:49 -0300

    fix: Repair bug

--- src/Components/MusicCard/index.jsx
+++ src/Components/MusicCard/index.jsx
@@ -1,22 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../Loading';
+import { addSong } from '../../services/favoriteSongsAPI';
 
 class index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorite: false,
+      isLoad: true,
+    };
+    this.handleFavorite = this.handleFavorite.bind(this);
+  }
+
+  handleFavorite() {
+    const { trackId } = this.props;
+    const { favorite } = this.state;
+
+    if (!favorite) {
+      this.setState({
+        favorite: true,
+        isLoad: false,
+      });
+      addSong(trackId).then(() => {
+        this.setState({
+          isLoad: true,
+        });
+      });
+    } else {
+      this.setState({ favorite: false });
+    }
+  }
+
   render() {
-    // console.log(this.props);
-    const { audio } = this.props;
+    const { favorite, isLoad } = this.state;
+    const { audio, trackId } = this.props;
     return (
-      <audio data-testid="audio-component" src={ audio } controls>
-        <track kind="captions" />
-        O seu navegador não suporta o elemento
-        <code>audio</code>
-      </audio>
+      <div>
+        { isLoad ? (
+          <div>
+            <audio data-testid="audio-component" src={ audio } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+            </audio>
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                onChange={ this.handleFavorite }
+                checked={ favorite }
+                type="checkbox"
+                id="favorite"
+                data-testid={ `checkbox-music-${trackId}` }
+              />
+            </label>
+          </div>) : <Loading />}
+      </div>
+
     );
   }
 }
 
 index.propTypes = {
   audio: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
 };
 
 export default index;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -4,20 +4,13 @@ import Header from '../Components/Header';
 import MusicCard from '../Components/MusicCard';
 import getMusics from '../services/musicsAPI';
 // import './albumStyle.css';
-import Loading from '../Components/Loading';
 
 export default class Album extends Component {
   constructor() {
     super();
     this.state = {
       album: [],
-      isLoad: false,
-      favorites: [],
     };
-
-    console.log(this);
-
-    this.handleFavorite = this.handleFavorite.bind(this);
   }
 
   componentDidMount() {
@@ -31,19 +24,8 @@ export default class Album extends Component {
     }));
   }
 
-  handleFavorite({ target }) {
-    console.dir(target.checked);
-    if (target.checked) {
-      this.setState({
-        isLoad: true,
-
-      });
-    };
-  }
-
   render() {
-    const { album, artistName, collectionName, trackId, isLoad } = this.state;
-
+    const { album, artistName, collectionName } = this.state;
     const validateAlbum = album.filter((musics) => musics.previewUrl !== undefined);
     return (
       <div data-testid="page-album">
@@ -52,25 +34,14 @@ export default class Album extends Component {
           <h2 data-testid="album-name">{collectionName}</h2>
           <h3 data-testid="artist-name">{artistName}</h3>
         </div>
-        { isLoad ? <Loading /> : (
-          <ul>
-            {validateAlbum.map(({ trackName, previewUrl }) => (
-              <li key={ trackName }>
-                <p>{trackName}</p>
-                <MusicCard audio={ previewUrl } />
-                <label htmlFor="favorite">
-                  Favorita:
-                  <input
-                    data-testid={ `checkbox-music-${trackId}` }
-                    onClick={ (e) => this.handleFavorite(e) }
-                    type="checkbox"
-                    name="favorite"
-                    id="favorite"
-                  />
-                </label>
-              </li>
-            ))}
-          </ul>)}
+        <ul>
+          {validateAlbum.map(({ trackName, previewUrl, trackId }) => (
+            <li key={ trackName }>
+              <p>{trackName}</p>
+              <MusicCard audio={ previewUrl } trackId={ trackId } />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
